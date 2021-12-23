4d931eae127a5deb2996b236e11c1a860a9a4922
Galbenarush <galbenarush@gmail.com>
2021-12-23 13:45:00 +0200

    Init commit

--- Presentation/App.js
+++ Presentation/App.js
@@ -1,51 +1,100 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Button, Block, Text, Input, theme } from 'galio-framework';
-
-// IMPLEMENT NOTIFICATIONS: https://www.youtube.com/watch?v=z6DEJXYQpP4&ab_channel=AdrianTwarog
-export default function App() {
-  return (
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
-    <Block style={styles.container}>
-      <Button onPress={ () => console.log("pressed") }>
-        <Block row middle>
-          <Text size={16} >TEST PRESS</Text>
-        </Block>
-      </Button>
-      <Button onPress={() => {
-        console.log("post sent")
-
-        // Set IP to your local IPv4.
-        fetch('http://172.16.28.12:8080/TEST', {
-          method: 'POST',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-              param1: 'stringParam',
-              param2: 12
-            })
-        });
-      }}>
-        <Block row middle>
-          <Text size={16} >TEST POST</Text>
-        </Block>
-      </Button>
-    </Block>
-  );
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { StyleSheet, View } from 'react-native';
+// import { Button, Block, Text, Input, theme } from 'galio-framework';
+// import CreateEvent from './Home/CreateEvent/CreateEvent';
+// import HomeScreen from './Home/HomeScreen';
+// // IMPLEMENT NOTIFICATIONS: https://www.youtube.com/watch?v=z6DEJXYQpP4&ab_channel=AdrianTwarog
+// export default function App() {
+//   return (
+//     // <View style={styles.container}>
+//     //   <Text>Open up App.js to start working on your app!</Text>
+//     //   <StatusBar style="auto" />
+//     // </View>
+//     <Block style={styles.container}>
+//       <CreateEvent />
+//     </Block>
+//   );
   
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text, Button, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import CreateEventScreen from './Home/CreateEvent/CreateEvent'
+import TestsScreen from './Tests/InitAppTests'
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{marginBottom: 20}}>Home Screen</Text>
+      <Button 
+        style={{marginBottom: 20}}
+        title="Create Event"
+        onPress={() =>
+          navigation.navigate('Create')}
+      ></Button>
+      <Button
+        style={{marginBottom: 20}} 
+        title="Join Event"
+        onPress={() =>
+          navigation.navigate('Join')}
+      ></Button>
+      <Button
+        style={{marginBottom: 20}} 
+        title="Some Tests"
+        onPress={() =>
+          navigation.navigate('Tests')}
+      ></Button>
+    </View>
+  );
+}
+function JoinEventScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Join Event Screen</Text>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// createNativeStackNavigator is a function that returns an object containing 2 properties: Screen and Navigator
+const Stack = createNativeStackNavigator();
+
+/*
+PROPS
+<Stack.Screen name="Home">
+  {props => <HomeScreen {...props} extraData={someData} />}
+</Stack.Screen>
+*/
+
+class App extends React.Component {
+
+    render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Welcome to Activitygram!' }} />
+          <Stack.Screen name="Create" component={CreateEventScreen} />
+          <Stack.Screen name="Join" component={JoinEventScreen} />
+          <Stack.Screen name="Tests" component={TestsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+
+}
+
+
+export default App;
---
+++ Presentation/Home/CreateEvent/CreateEvent.js
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import { View, Text, Button, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// createNativeStackNavigator is a function that returns an object containing 2 properties: Screen and Navigator
+const Stack = createNativeStackNavigator();
+
+class CreateEvent extends React.Component {
+  constructor()
+  {
+    super();
+    this.state={
+      name: '',
+      email: '',
+      password: '',
+    }
+  }
+
+  submit()
+  {
+    console.warn(this.state)
+  }
+    render() {
+    return (
+      <View style={{margin: 20}}>
+        <Text style={{margin: 20, marginTop: 50 }}>Create Event</Text>
+        <TextInput
+          placeholder='Choose Event type'
+          onChangeText={(text) => {this.setState({name: text})}}
+          style={{borderWidth: 2, borderColor: 'skyblue', margin: 20}}
+        ></TextInput>
+        <TextInput
+          placeholder='Enter Event name'
+          onChangeText={(text) => {this.setState({email: text})}}
+          style={{borderWidth: 2, borderColor: 'skyblue', margin: 20}}
+        ></TextInput>
+        <TextInput
+          placeholder='Enter Description'
+          onChangeText={(text) => {this.setState({password: text})}}
+          style={{borderWidth: 2, borderColor: 'skyblue', margin: 20}}
+        ></TextInput>
+        <TextInput
+          placeholder='Enter Password'
+          onChangeText={(text) => {this.setState({password: text})}}
+          style={{borderWidth: 2, borderColor: 'skyblue', margin: 20}}
+        ></TextInput>
+        <TextInput
+          placeholder='Enter Password'
+          onChangeText={(text) => {this.setState({password: text})}}
+          style={{borderWidth: 2, borderColor: 'skyblue', margin: 20}}
+        ></TextInput>
+        <Button title="Publish" onPress={()=>{this.submit()}} />
+      </View>
+    );
+  }
+
+}
+
+export default CreateEvent;
