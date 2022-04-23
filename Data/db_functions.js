168ba9376055cc50241af1a3b3c9556693ecfbbd
Shir <shirzituni@gmail.com>
2022-04-23 13:49:25 +0300

    function for update user info

--- Data/db_functions.js
+++ Data/db_functions.js
@@ -1,36 +1,38 @@
 const User = require("./User.js");
 const { MongoClient } = require('mongodb');
 
-
+// print databases
  async function listDatabases(client) {
     databasesList = await client.db().admin().listDatabases();
     console.log("Databases:");
     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 };
-async function changeUserName(client, newUser){
-    // See https://mongodb.github.io/node-mongodb-native/3.6/api/Collection.html#insertOne for the insertOne() docs
-    const result = await client.db("activitygram").collection("users").insertOne(newUser.forDB);
+
+async function changeUserName(client, curr_user, new_name){
+    const result = await client.db("activitygram").collection("users").updateOne(
+        {
+            // used to filter the document
+            "_id" : curr_user.id
+        },
+        {
+        $set:{
+            firstName: new_name
+            }
+        }
+);
     console.log(`New listing created with the following id: ${result.insertedId}`);
 }
 
+//creat newUser
 async function createNewUser(client, newUser){
-    // See https://mongodb.github.io/node-mongodb-native/3.6/api/Collection.html#insertOne for the insertOne() docs
     const result = await client.db("activitygram").collection("users").insertOne(newUser.forDB);
+    console.log(newUser.id)
+    newUser.set_id = result.insertedId;
+    console.log(newUser.id)
     console.log(`New listing created with the following id: ${result.insertedId}`);
 }
 async function main() {
-    /**
-     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-     */
-     const uri = "mongodb+srv://shirziruni:Activitygram@cluster0.jjvsr.mongodb.net/Cluster0?retryWrites=true&w=majority";
-    /**
-     * The Mongo Client you will use to interact with your database
-     * See https://mongodb.github.io/node-mongodb-native/3.6/api/MongoClient.html for more details
-     * In case: '[MONGODB DRIVER] Warning: Current Server Discovery and Monitoring engine is deprecated...'
-     * pass option { useUnifiedTopology: true } to the MongoClient constructor.
-     * const client =  new MongoClient(uri, {useUnifiedTopology: true})
-     */
+    const uri = "mongodb+srv://shirziruni:Activitygram@cluster0.jjvsr.mongodb.net/Cluster0?retryWrites=true&w=majority";
     const client = new MongoClient(uri);
 
     try {
@@ -39,7 +41,7 @@ async function main() {
         new_user = new User()
         // Create a single new listing
         await createNewUser(client, new_user);
-                            
+        await changeUserName(client, new_user, "SHLOMI");
         } catch (e) {
         console.error(e);
     } finally {
