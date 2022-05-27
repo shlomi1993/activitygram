074b4c546ed094e630ab2ba5d9c1d858d05c5170
Galbenarush <galbenarush@gmail.com>
2022-05-27 15:40:51 +0300

    new components

---
+++ Presentation/Activitygram_Client/src/components/index.tsx
@@ -0,0 +1,19 @@
+import Block from './Block';
+import Button from './Button';
+import Input from './Input';
+import Text from './Text';
+import Modal from './Modal';
+import Switch from './Switch';
+import Checkbox from './Checkbox';
+import Image from './Image';
+
+export {
+  Block,
+  Button,
+  Input,
+  Text,
+  Switch,
+  Checkbox,
+  Image,
+  Modal,
+};
