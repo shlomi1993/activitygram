31dff5ca6e3be1675f0680fa8fd88b8f548ef9f6
Galbenarush <galbenarush@gmail.com>
2022-05-29 19:18:17 +0300

    screens templates and search screen

---
+++ Presentation/Activitygram_Client/src/newScreens/Profile.tsx
@@ -0,0 +1,35 @@
+import React, {useLayoutEffect, useState, useCallback } from 'react';
+
+import {useNavigation} from '@react-navigation/native';
+import {useHeaderHeight} from '@react-navigation/stack';
+
+import {useTheme } from '../hooks';
+import {Block, Image } from '../components';
+import 'react-native-gesture-handler';
+
+const Profile = () => {
+  const {assets, sizes } = useTheme();
+  const navigation = useNavigation();
+  const headerHeight = useHeaderHeight();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerBackground: () => (
+        <Image
+          radius={0}
+          resizeMode="cover"
+          width={sizes.width}
+          height={headerHeight}
+          source={assets.background}
+        />
+      ),
+    });
+  }, [assets.background, navigation, sizes.width, headerHeight]);
+
+  return (
+    <Block safe>
+    </Block>
+  );
+};
+
+export default Profile;
