c0728ff6624981ffe4ad24846fbc67d6662f014b
shlomi1993 <shlomiggl@gmail.com>
2022-05-31 13:55:46 +0300

    try to login with google

--- Presentation/Activitygram_Client/firebase.js
+++ Presentation/Activitygram_Client/firebase.js
@@ -5,22 +5,22 @@ import 'firebase/compat/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAr9UG-0zZexLpB9vwUv5fwUf2j6Cop03U",
-  authDomain: "activitygram-auth.firebaseapp.com",
-  projectId: "activitygram-auth",
-  storageBucket: "activitygram-auth.appspot.com",
-  messagingSenderId: "78872694554",
-  appId: "1:78872694554:web:e6a5321c7f81ca40ea26a3"
+	apiKey: 'AIzaSyAr9UG-0zZexLpB9vwUv5fwUf2j6Cop03U',
+	authDomain: 'activitygram-auth.firebaseapp.com',
+	projectId: 'activitygram-auth',
+	storageBucket: 'activitygram-auth.appspot.com',
+	messagingSenderId: '78872694554',
+	appId: '1:78872694554:web:e6a5321c7f81ca40ea26a3'
 };
 
 // Initialize Firebase
 let app;
 if (firebase.apps.length === 0) {
-  app = firebase.initializeApp(firebaseConfig);
+	app = firebase.initializeApp(firebaseConfig);
 } else {
-  app = firebase.app()
+	app = firebase.app();
 }
 
-const auth = firebase.auth()
+const auth = firebase.auth();
 
 export { auth };
