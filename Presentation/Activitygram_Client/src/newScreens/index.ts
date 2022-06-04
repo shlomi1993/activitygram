c2f46959e2a78b26fe3d6f42d7fa4c8c9c0bb664
Galbenarush <galbenarush@gmail.com>
2022-06-04 11:21:22 +0300

    Activity screen

--- Presentation/Activitygram_Client/src/newScreens/index.ts
+++ Presentation/Activitygram_Client/src/newScreens/index.ts
@@ -4,3 +4,4 @@ export {default as Profile} from './Profile';
 export {default as Explore} from './Explore';
 export {default as Post} from './Post';
 export {default as Search} from './Search';
+export {default as Activity} from './Activity';
